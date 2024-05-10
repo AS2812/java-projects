7adb7be3308cbd79577b01fc2fa66f4881662efc
Ahmed Shacker <ahmedchacker@gmail.com>
2024-05-10 20:21:37 +0300

    Update Ordermanager-class.java

--- data-sructure-projects/application restaurant system on data structure/Ordermanager-class.java
+++ data-sructure-projects/application restaurant system on data structure/Ordermanager-class.java
@@ -1,44 +1,38 @@
 import java.util.ArrayList;
-import java.util.*;
-class OrderManager {
+import java.util.Collections;
+import java.util.List;
+
+class OrderManager  {
     private List<Order> orders;
 
-    public OrderManager()
-    {
+    public OrderManager() {
         orders = new ArrayList<>();
     }
-
 
     public void placeOrder(Order order) {
         orders.add(order);
         System.out.println("Order placed successfully. Order number: " + order.getOrderNumber());
     }
 
-
-    public void cancelOrder(int orderNumber)
-    {
+    public void cancelOrder(int orderNumber) {
         Order order = findOrder(orderNumber);
-        if (order != null)
-        {
+        if (order != null) {
             orders.remove(order);
             System.out.println("Order cancelled successfully.");
-        }
-        else
-        {
+        } else {
             System.out.println("Order not found.");
         }
     }
 
+    public Order findOrder(int orderNumber) {
+        // Sort the list before performing binary search
+        Collections.sort(orders, new Ordercompartor());
 
-    public Order findOrder(int orderNumber)
-    {
-        for (Order order : orders)
-        {
-            if (order.getOrderNumber() == orderNumber)
-            {
-                return order;
-            }
+        int index = Collections.binarySearch(orders, new Order(orderNumber), new Ordercompartor());
+        if (index >= 0) {
+            return orders.get(index);
+        } else {
+            return null;
         }
-        return null;
     }
 }
