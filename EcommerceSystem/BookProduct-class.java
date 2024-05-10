20e91e17ef6779336c4fec01a7d5800f80005cca
ahmed shacker <ahmedchacker@gmail.com>
2024-05-10 10:01:41 +0300

    Create BookProduct-class.java

---
+++ EcommerceSystem/BookProduct-class.java
@@ -0,0 +1,61 @@
+public class BookProduct extends Product
+{
+    protected String author;
+    protected String publisher;
+    BookProduct(int id , float price , String name , String author , String publisher)
+    {
+        super(id , price , name);
+        this.author=author;
+        this.publisher=publisher;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public void setAuthor(String author)
+    {
+        this.author = author;
+    }
+
+    public String getPublisher()
+    {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher)
+    {
+        this.publisher = publisher;
+    }
+
+    public int getProductId()
+    {
+        return productId;
+    }
+
+    public void setProductId(int productId)
+    {
+        this.productId = productId;
+    }
+
+    public float getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(float price)
+    {
+        this.price = price;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
