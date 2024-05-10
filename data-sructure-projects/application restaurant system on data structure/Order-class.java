ee6934303b9b82dae55f705703436d94031a84a9
Ahmed Shacker <ahmedchacker@gmail.com>
2024-05-10 21:29:59 +0300

    Update Order-class.java

--- data-sructure-projects/application restaurant system on data structure/Order-class.java
+++ data-sructure-projects/application restaurant system on data structure/Order-class.java
@@ -3,44 +3,40 @@ class Order {
     private int orderNumber;
     private List<OrderItem> items;
 
-    public Order(int orderNumber)
-    {
+    public Order(int orderNumber) {
         this.orderNumber = orderNumber;
         items = new ArrayList<>();
     }
 
-
-    public void addItem(String itemName, int quantity, double price)
-    {
+    public void addItem(String itemName, int quantity, double price) {
         items.add(new OrderItem(itemName, quantity, price));
     }
 
-
-    public void removeItem(String itemName)
-    {
+    public void removeItem(String itemName) {
         items.removeIf(item -> item.getItemName().equals(itemName));
     }
 
-
-    public double calculateTotalCost(double taxRate, double gratuityRate) {
-        double subtotal = 0;
-        for (OrderItem item : items)
-        {
-            subtotal += item.calculateSubtotal();
+    public double calculateTotalPrice() {
+        double totalPrice = 0;
+        for (OrderItem item : items) {
+            totalPrice += item.calculateSubtotal();
         }
-        double tax = subtotal * taxRate;
-        double gratuity = subtotal * gratuityRate;
-        return subtotal + tax + gratuity;
+        return totalPrice;
     }
 
+    public void displayOrderDetails() {
+        System.out.println("Order Number: " + orderNumber);
+        System.out.println("Items:");
+        for (OrderItem item : items) {
+            System.out.println("- " + item.getItemName() + " | Quantity: " + item.getQuantity() + " | Price: $" + item.getPrice());
+        }
+    }
 
-    public int getOrderNumber()
-    {
+    public int getOrderNumber() {
         return orderNumber;
     }
 
-    public List<OrderItem> getItems()
-    {
+    public List<OrderItem> getItems() {
         return items;
     }
 }
