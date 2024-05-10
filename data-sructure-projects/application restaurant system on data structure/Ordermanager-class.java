41d552dfe88b8b7bed5602e6d8b69ffd1a8ebca6
Ahmed Shacker <ahmedchacker@gmail.com>
2024-05-10 18:47:35 +0300

    Create Ordermanager-class.java

---
+++ data-sructure-projects/application restaurant system on data structure/Ordermanager-class.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.*;
+class OrderManager {
+    private List<Order> orders;
+
+    public OrderManager()
+    {
+        orders = new ArrayList<>();
+    }
+
+
+    public void placeOrder(Order order) {
+        orders.add(order);
+        System.out.println("Order placed successfully. Order number: " + order.getOrderNumber());
+    }
+
+
+    public void cancelOrder(int orderNumber)
+    {
+        Order order = findOrder(orderNumber);
+        if (order != null)
+        {
+            orders.remove(order);
+            System.out.println("Order cancelled successfully.");
+        }
+        else
+        {
+            System.out.println("Order not found.");
+        }
+    }
+
+
+    public Order findOrder(int orderNumber)
+    {
+        for (Order order : orders)
+        {
+            if (order.getOrderNumber() == orderNumber)
+            {
+                return order;
+            }
+        }
+        return null;
+    }
+}
