d4e51c7714b1c5649cce4a4c7ed6e31d3bffa55d
Ahmed Shacker <ahmedchacker@gmail.com>
2024-05-10 21:29:46 +0300

    Update Menumanager-class.java

--- data-sructure-projects/application restaurant system on data structure/Menumanager-class.java
+++ data-sructure-projects/application restaurant system on data structure/Menumanager-class.java
@@ -1,74 +1,71 @@
 import java.util.*;
+
 class MenuManager {
-    private Map<String, MenuItem> menu;
+    private static Map<String, MenuItem> menu;
 
     public MenuManager()
     {
         menu = new HashMap<>();
     }
 
-
-    public void addMenu(String name, double price, String description, String category)
-    {
+    public void addMenu(String name, double price, String description, String category) {
         MenuItem newItem = new MenuItem(name, price, description, category);
         menu.put(name, newItem);
     }
 
     public void updateMenu(String name, double price, String description, String category) {
-        if (menu.containsKey(name))
-        {
+        if (menu.containsKey(name)) {
             MenuItem item = menu.get(name);
             item.setPrice(price);
             item.setDescription(description);
             item.setCategory(category);
-        }
-        else
-        {
+        } else {
             System.out.println("Item not found in the menu.");
         }
     }
 
-
-    public void removeItem(String name)
-    {
-        if (menu.containsKey(name))
-        {
+    public void removeItem(String name) {
+        if (menu.containsKey(name)) {
             menu.remove(name);
             System.out.println("Item removed successfully.");
-        }
-        else
-        {
+        } else {
             System.out.println("Item not found in the menu.");
         }
     }
 
-
-    public MenuItem searchName(String name)
+    public static MenuItem searchName(String name)
     {
         return menu.get(name);
     }
 
-
-    public List<MenuItem> searchCategory(String category)
-    {
+    public List<MenuItem> searchCategory(String category) {
         List<MenuItem> result = new ArrayList<>();
         for (MenuItem item : menu.values()) {
-            if (item.getCategory().equalsIgnoreCase(category))
-            {
+            if (item.getCategory().equalsIgnoreCase(category)) {
                 result.add(item);
             }
         }
         return result;
     }
 
-
-    public void displayMenu()
-    {
-        for (MenuItem item : menu.values())
-        {
-            System.out.println(item);
+    public static void displayMenu() {
+        if (menu.isEmpty()) {
+            System.out.println("Menu is empty.");
+        } else {
+            System.out.println("Menu:");
+            for (MenuItem item : menu.values()) {
+                System.out.println(item);
+            }
         }
     }
-
+     public static MenuItem getMenuItem(int menuItemNumber) {
+        // Adjust index to match List index (subtract 1 because menu items are 1-indexed)
+        int index = menuItemNumber - 1;
+        if (index >= 0 && index < menu.size()) {
+            return menu.get(index);
+        } else {
+            return null; // Return null if the index is out of bounds
+        }
     }
+}
 
