4eed9284dae895d5ec9318d9958a30fcda91edd0
Ahmed Shacker <ahmedchacker@gmail.com>
2024-05-10 21:46:16 +0300

    Create Staff-class.java

---
+++ hotel-system/Staff-class.java
@@ -0,0 +1,10 @@
+
+public class staff {
+      String staffID;
+    String password;
+    staff(String staffID, String password){
+        this.staffID = staffID;
+        this.password = password;
+    
+}
+}
